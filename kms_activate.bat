af84a27f60c4ce8238a9ea476f5038e9fe985e7e
lewis <lewis19932@163.com>
2020-01-13 11:20:49 +0800

    init

---
+++ kms_activate.bat
@@ -0,0 +1,5 @@
+slmgr.vbs -upk
+slmgr.vbs -ipk FJ82H-XT6CR-J8D7P-XQJJ2-GPDD4      #不同系统版本这里的激活码不一样，请往下翻寻找表格！
+slmgr.vbs -skms kms.cangshui.net      #这是我的Kms域名，如果你自己的已搭建好，可以输入你的域名或者IP                     
+slmgr.vbs -ato
+slmgr.vbs -dlv
